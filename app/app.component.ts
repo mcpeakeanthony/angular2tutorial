d32a598dcc42df44fe128523843a373f44b1d941
Anthony McPeake <Anthony.McPeake@valtech.co.uk>
2016-05-09 22:29:20 +0100

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by anthonymcpeake on 09/05/2016.
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My Secondfffff Angular 2 Ap321p</h1>'
+})
+export class AppComponent { }
---
+++ app/main.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by anthonymcpeake on 09/05/2016.
+ */
+
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+
+import { AppComponent } from './app.component';
+
+bootstrap(AppComponent);
